aa700eaa86e597dc537d3f2476f4f3a69e847f4c
Philipp Benner <benner@molgen.mpg.de>
2019-06-05 12:20:50 +0200

    2019/06/05-12:20:50 (Linux sirius unknown)

---
+++ kmerLr_crossvalidation.go
@@ -0,0 +1,45 @@
+/* Copyright (C) 2019 Philipp Benner
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package main
+
+/* -------------------------------------------------------------------------- */
+
+//import "fmt"
+import "math/rand"
+
+/* -------------------------------------------------------------------------- */
+
+func shuffleInts(vals []int, seed int64) {
+  r := rand.New(rand.NewSource(seed))
+  for len(vals) > 0 {
+    n := len(vals)
+    randIndex := r.Intn(n)
+    vals[n-1], vals[randIndex] = vals[randIndex], vals[n-1]
+    vals = vals[:n-1]
+  }
+}
+
+func getCvGroups(n, fold int, seed int64) []int {
+  groups := make([]int, n)
+  for i := 0; i < n; i += fold {
+    for j := 0; j < fold && i+j < n; j++ {
+      groups[i+j] = j
+    }
+  }
+  shuffleInts(groups, seed)
+  return groups
+}
