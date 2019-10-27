3f46713f25bb9f3edbbfaec7850b3f97da328c53
Philipp Benner <benner@molgen.mpg.de>
2019-10-27 16:52:59 +0100

    2019/10/27-16:52:59 (Linux sirius unknown)

--- kmerLr_test.go
+++ kmerLr_test.go
@@ -40,7 +40,7 @@ func Test1(test *testing.T) {
     features  = append(features, [2]int{5068 , 5486 }) // aaagaaa|tttcttt   = 1, aagannt|anntctt   = 7
     features  = append(features, [2]int{19270, 57071}) // aacgcgna|tncgcgtt = 1, tgaatgca|tgcattca = 1
     features  = append(features, [2]int{4671 , 5486 }) // gntanc|gntanc     = 3, aagannt|anntctt   = 7
-    
+
     data2, _, kmers := compile_training_data(config, kmersCounter, kmers, features, "kmerLr_test.fa", "kmerLr_test.fa")
 
     for i, _ := range features[0:len(features)-4] {
@@ -74,6 +74,36 @@ func Test2(test *testing.T) {
   kmersCounter, err := NewKmerCounter(4, 8, false, false, true, nil, GappedNucleotideAlphabet{}); if err != nil {
     test.Error(err)
   } else {
+    data1, _, kmers := compile_training_data(config, kmersCounter, nil, nil, "kmerLr_test.fa", "kmerLr_test.fa")
+
+    features := newFeatureIndices(len(kmers), false)
+
+    kmersCounter, _ = NewKmerCounter(4, 8, false, false, true, nil, GappedNucleotideAlphabet{}, kmers...)
+    data2, _, kmers := compile_training_data(config, kmersCounter, nil, nil, "kmerLr_test.fa", "kmerLr_test.fa")
+
+    if data1[0].Dim() != data2[0].Dim() {
+      test.Error("test failed")
+    } else {
+      for i, _ := range features {
+        if data1[0].ValueAt(i+1) != data2[0].ValueAt(i+1) {
+          test.Error("test failed")
+        }
+        if data1[1].ValueAt(i+1) != data2[1].ValueAt(i+1) {
+          test.Error("test failed")
+        }
+      }
+    }
+  }
+}
+
+func Test3(test *testing.T) {
+  config := Config{}
+  //config.Binarize = true
+  config.Verbose  = 0
+
+  kmersCounter, err := NewKmerCounter(4, 8, false, false, true, nil, GappedNucleotideAlphabet{}); if err != nil {
+    test.Error(err)
+  } else {
     _, _, kmers := compile_training_data(config, kmersCounter, nil, nil, "kmerLr_test.fa", "kmerLr_test.fa")
     kmers = kmers[0:10]
     data1, _,  _ := compile_training_data(config, kmersCounter, kmers, nil, "kmerLr_test.fa", "kmerLr_test.fa")
@@ -93,7 +123,7 @@ func Test2(test *testing.T) {
   }
 }
 
-func Test3(test *testing.T) {
+func Test4(test *testing.T) {
   config := Config{}
   config.Verbose        = 0
   config.MaxEpochs      = 100
